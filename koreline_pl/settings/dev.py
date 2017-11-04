ec9d3819f35d9fc13786db64106df1f02f0bc09c
Dawid Rdzanek <dawid.rdzanek@1connect.pl>
2017-11-04 22:59:52 +0100

    first commit

---
+++ koreline_pl/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'koreline',
+        'USER': 'akus',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
